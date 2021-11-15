c77a525da572aa56e46f670bff06065b3274acb7
Chris Ho <cho@dhcp-v058-122.mobile.uci.edu>
2021-11-14 17:39:27 -0800

    Created executable training script

--- src/python/BridgeDataModule.py
+++ src/python/BridgeDataModule.py
@@ -1,5 +1,7 @@
 import pytorch_lightning as pl
+from typing import Optional
 from torch.utils.data import random_split, DataLoader, Dataset
+import torch.nn.functional as F
 import numpy as np
 import pyspiel
 import torch
@@ -27,27 +29,24 @@ def _no_play_trajectory(line: str):
 class BridgeDataset(Dataset):
     def __init__(self, filename : str):
         self.filename = filename
-        self.all_trajectories = [_no_play_trajectory(line) for line in open(self.data_dir)]
+        self.all_trajectories = [_no_play_trajectory(line) for line in open(self.filename)]
 
     def __len__(self):
         return len(self.all_trajectories)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
 
-        obs = np.zeros([len(idx)] + GAME.observation_tensor_shape(), np.float32)
-        labels = np.zeros(len(idx), dtype=np.int32)
+#         obs = np.zeros([1,] + GAME.observation_tensor_shape(), np.float32)
+#         labels = np.zeros([1,], dtype=np.int32)
 
-        for i in range(len(idx)):
-            traj = self.all_trajectories[idx[i]]
-            state = GAME.new_initial_state()
-            action_index = np.random.randint(52, len(self.all_trajectories[i]))
-            for action in traj[:action_index]:
-                state.apply_action(action)
+        traj = self.all_trajectories[idx]
+        state = GAME.new_initial_state()
+        action_index = np.random.randint(52, len(self.all_trajectories[idx]))
+        for action in traj[:action_index]:
+            state.apply_action(action)
 
-            obs[i] = state.observation_tensor()
-            labels[i] = self.traj[action_index]
+        obs = torch.tensor(state.observation_tensor())
+        labels = torch.tensor(traj[action_index]) - MIN_ACTION
 
         return {'observation' : obs, 'labels' : labels}
 
@@ -57,20 +56,23 @@ class BridgeDataModule(pl.LightningDataModule):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
-        self.bridge_data = {}
+        self.bridge_paths = {}
 
     def setup(self, stage: Optional[str] = None):
         for mode in ['train', 'test', 'valid']:
-            self.bridge_data[mode] = BridgeDataset(f'{self.data_dir}_{mode}')
+            self.bridge_paths[mode] = f'{self.data_dir}/{mode}.txt'
 
     def train_dataloader(self):
-        return DataLoader(self.bridge_data['train'], batch_size=self.batch_size)
+        return DataLoader(BridgeDataset(self.bridge_paths['train']), 
+                          batch_size=self.batch_size)
 
     def val_dataloader(self):
-        return DataLoader(self.bridge_data['valid'], batch_size=self.batch_size)
+        return DataLoader(BridgeDataset(self.bridge_paths['valid']), 
+                          batch_size=self.batch_size)
 
     def test_dataloader(self):
-        return DataLoader(self.bridge_data['test'], batch_size=self.batch_size)
+        return DataLoader(BridgeDataset(self.bridge_paths['test']), 
+                          batch_size=self.batch_size)
 
     def teardown(self, stage: Optional[str] = None) -> None:
         pass
--- src/python/BridgeNetwork.py
+++ src/python/BridgeNetwork.py
@@ -1,18 +1,24 @@
 import pytorch_lightning as pl
 from torch.utils.data import random_split, DataLoader, Dataset
 import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+NUM_ACTIONS = 38 #89 - 52 + 1
+MIN_ACTION = 52
 
 class RFC(pl.LightningModule):
     def __init__(self, dim):
         super().__init__()
 
         # RFCs should connect
-        self.fc1 = torch.nn.Linear(dim, dim)
-        self.fc2 = torch.nn.Linear(dim, dim)
+        self.fc1 = nn.Linear(dim, dim)
+        self.fc2 = nn.Linear(dim, dim)
 
     def forward(self, x):
-        l1 = torch.relu(self.fc1(x))
-        l2 = torch.relu(self.fc2(l1))
+        l1 = F.relu(self.fc1(x))
+        l2 = F.relu(self.fc2(l1))
 
         return l2 + x
 
@@ -20,11 +26,11 @@ class RFC(pl.LightningModule):
 class RFCBackbone(pl.LightningModule):
     def __init__(self, input_dim = 571, inner_dim = 256, num_blocks = 2):
         super().__init__()
-        self.fc1 = torch.nn.Linear(input_dim, inner_dim)
+        self.fc1 = nn.Linear(input_dim, inner_dim)
         self.rfc1 = [RFC(inner_dim) for i in range(2)]
-        self.efc = torch.nn.Linear(inner_dim, input_dim)
+        self.efc = nn.Linear(inner_dim, input_dim)
 
-        self.fc2 = torch.nn.Linear(input_dim * 2, inner_dim)
+        self.fc2 = nn.Linear(2*input_dim, inner_dim)
         self.rfc2 = [RFC(inner_dim) for i in range(2)]
 
     def forward(self, x):
@@ -34,7 +40,7 @@ class RFCBackbone(pl.LightningModule):
 
         x1 = self.efc(x1)
 
-        x_modified = torch.cat(x, x1)
+        x_modified = torch.cat([x, x1], dim=1)
 
         x2 = self.fc2(x_modified)
         for rfc in self.rfc2:
@@ -48,7 +54,39 @@ class BridgeSupervised(pl.LightningModule):
         super().__init__()
         self.inner_dim = inner_dim
         self.backbone = RFCBackbone(input_dim, inner_dim, num_blocks)
-        self.out = torch.nn.Linear(inner_dim, 1)
+        self.out = nn.Linear(inner_dim, NUM_ACTIONS)
+
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.metrics_fn = lambda yhat,y: {'acc' : (yhat == y).float().mean()}
+
+        self.save_hyperparameters()
 
     def forward(self, x):
+        '''
+        Outputs one-hot encoding of action.
+        Use .argmax(dim=1) + MIN_ACTION to get index per minibatch.
+        '''
         x = self.backbone(x)
+        x = F.softmax(self.out(x), dim=1)
+        return x + MIN_ACTION
+    
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=1e-4)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch['observation'], batch['labels']
+        yhat = self.forward(x)
+
+        loss = self.loss_fn(yhat, y)
+        metrics = self.metrics_fn(yhat.argmax(dim=1), y)
+
+        return {'loss' : loss, **metrics}
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch['observation'], batch['labels']
+        yhat = self.forward(x)
+
+        loss = self.loss_fn(yhat, y)
+        metrics = self.metrics_fn(yhat.detach().argmax(dim=1), y.detach())
+
+        return {'loss' : loss, **metrics}
