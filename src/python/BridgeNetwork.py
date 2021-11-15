9910583c6e11300a1376f476c016110dca60f439
Chris Ho <chriswxho@gmail.com>
2021-11-14 21:17:15 -0800

    Edited model file to work on GPU

--- src/python/BridgeNetwork.py
+++ src/python/BridgeNetwork.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 NUM_ACTIONS = 38 #89 - 52 + 1
 MIN_ACTION = 52
 
-class RFC(pl.LightningModule):
+class RFC(nn.Module):
     def __init__(self, dim):
         super().__init__()
 
@@ -17,34 +17,33 @@ class RFC(pl.LightningModule):
         self.fc2 = nn.Linear(dim, dim)
 
     def forward(self, x):
-        l1 = F.relu(self.fc1(x))
-        l2 = F.relu(self.fc2(l1))
+        identity = x
+        x1 = self.fc1(x)
+        x1 = F.relu(x1)
+        x1 = F.relu(self.fc2(x1))
 
-        return l2 + x
-
+        return x1 + identity
 
 class RFCBackbone(pl.LightningModule):
     def __init__(self, input_dim = 571, inner_dim = 256, num_blocks = 2):
         super().__init__()
         self.fc1 = nn.Linear(input_dim, inner_dim)
-        self.rfc1 = [RFC(inner_dim) for i in range(2)]
+        self.rfc1 = nn.Sequential(*[RFC(inner_dim) for i in range(2)])
         self.efc = nn.Linear(inner_dim, input_dim)
 
         self.fc2 = nn.Linear(2*input_dim, inner_dim)
-        self.rfc2 = [RFC(inner_dim) for i in range(2)]
+        self.rfc2 = nn.Sequential(*[RFC(inner_dim) for i in range(2)])
 
     def forward(self, x):
         x1 = self.fc1(x)
-        for rfc in self.rfc1:
-            x1 = rfc(x1)
+        x1 = self.rfc1(x1)
 
         x1 = self.efc(x1)
 
         x_modified = torch.cat([x, x1], dim=1)
 
         x2 = self.fc2(x_modified)
-        for rfc in self.rfc2:
-            x2 = rfc(x2)
+        x2 = self.rfc2(x2)
 
         return x2 #return the latent dimension, final predict values come later
 
